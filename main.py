e5a0fc63d31aed3c8bdcb599465d8cae323b7b57
Henry Li <xli257@jhu.edu>
2022-05-31 15:09:54 -0400

    implement distance cost calculation

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import numpy as np
-from utils import clear_plot, pick_starting_location, pick_plot
+import time
+import heapq
+from utils import clear_plot, pick_starting_location, pick_plot, get_adjacent_points, get_path_cost
 from gdpc import interface as INTF
 from gdpc import worldLoader as WL
 
+time_start = time.time()
 STARTX, STARTY, STARTZ, ENDX, ENDY, ENDZ = INTF.requestPlayerArea(dx = 128, dz = 128)
 print("Build area: ", STARTX, STARTY, STARTZ, ENDX, ENDY, ENDZ)
 
 WORLDSLICE = WL.WorldSlice(STARTX, STARTZ, ENDX + 1, ENDZ + 1)
 def get_geography_map(world_slice):
     sea_map = (world_slice.heightmaps['OCEAN_FLOOR'] == world_slice.heightmaps['MOTION_BLOCKING_NO_LEAVES']).astype(int)
+    # 1: land, 0: sea
     return sea_map
 
 height_map = np.minimum(WORLDSLICE.heightmaps['OCEAN_FLOOR'], WORLDSLICE.heightmaps['MOTION_BLOCKING_NO_LEAVES'])
+surface_map = WORLDSLICE.heightmaps['MOTION_BLOCKING_NO_LEAVES']
 
 sea_map = get_geography_map(WORLDSLICE)
 
 
 # pick starting position
 x_start, y_start, z_start = pick_starting_location(height_map, sea_map, STARTX, STARTZ, ENDX, ENDZ)
-
-
 # build starter house
 house_size = (9, 9)
 ## locate starter plot
@@ -27,3 +30,55 @@ house_area, house_level = pick_plot(house_size, height_map, STARTX, STARTZ, x_st
 ## clear plot
 clear_plot(house_area, house_level, STARTY, ENDY)
 ## build house
+
+## display elapsed time
+time_starter_house = time.time()
+print("Starter house build: ", time_starter_house - time_start)
+
+
+# build settlement
+## score map
+score_map = np.full(sea_map.shape, fill_value = -1)
+seafaring_cost = 5
+base_edge_cost = 1
+
+### get edge costs
+#### get sea edge costs
+sea_edge_0 = (np.absolute(sea_map[1:] - sea_map[:-1]) == 1).astype(int)
+sea_edge_1 = (np.absolute(sea_map[:, 1:] - sea_map[:, :-1]) == 1).astype(int)
+
+surface_edge_0 = np.absolute(surface_map[1:] - surface_map[:-1]) + sea_edge_0 * seafaring_cost
+surface_edge_1 = np.absolute(surface_map[:, 1:] - surface_map[:, :-1]) + sea_edge_1 * seafaring_cost
+
+edge_costs = [surface_edge_0, surface_edge_1]
+### run dijkstra
+score_map[house_area[0, 0] - STARTX + 1: house_area[0, 1] - STARTX + 1, house_area[1, 0] - STARTZ + 1: house_area[1, 1] - STARTZ + 1] = 0
+#!! assumption: all buildings are rectangular
+#### initialise point stack
+point_stack = []
+for x_house in range(house_area[0, 0], house_area[0, 1] + 1):
+    if x_house == house_area[0, 0] or x_house == house_area[0, 1]:
+        for y_house in range(house_area[1, 0], house_area[1, 1] + 1):
+            point_stack.append((0, x_house, y_house))
+    else:
+        point_stack.append((0, x_house, house_area[1, 0]))
+        point_stack.append((0, x_house, house_area[1, 1]))
+heapq.heapify(point_stack)
+#### pop from point stack
+while len(point_stack) > 0:
+    next_point = heapq.heappop(point_stack)
+    adjacent_point_list = np.asarray(get_adjacent_points(next_point[1], next_point[2], STARTX, STARTZ, ENDX, ENDZ))
+    adjacent_point_differential = adjacent_point_list - np.array([next_point[1], next_point[2]])
+    for adjacent_point_index, adjacent_point in enumerate(adjacent_point_list):
+        new_cost = get_path_cost(next_point[1] - STARTX, next_point[2] - STARTZ, adjacent_point_differential[adjacent_point_index], edge_costs) + base_edge_cost + next_point[0]
+        
+        if score_map[adjacent_point[0] - STARTX, adjacent_point[1] - STARTZ] == -1 or score_map[adjacent_point[0] - STARTX, adjacent_point[1] - STARTZ] > new_cost:
+            old_point = (score_map[adjacent_point[0] - STARTX, adjacent_point[1] - STARTZ], adjacent_point[0], adjacent_point[1])
+            new_point = (new_cost, adjacent_point[0], adjacent_point[1])
+            if old_point in point_stack:
+                point_stack[point_stack.index(old_point)] = new_point
+            else:
+                heapq.heappush(point_stack, new_point)
+            heapq.heapify(point_stack)
+            score_map[adjacent_point[0] - STARTX, adjacent_point[1] - STARTZ] = new_cost
+
--- utils.py
+++ utils.py
@@ -10,23 +10,22 @@ def pick_starting_location(height_map, sea_map, STARTX, STARTZ, ENDX, ENDZ):
         x_start = random.randint(STARTX, ENDX)
         z_start = random.randint(STARTZ, ENDZ)
         
-        if sea_map[x_start + 1 - STARTX, z_start + 1 - STARTZ] == 1:
+        if sea_map[x_start - STARTX, z_start - STARTZ] == 1:
             pick_location_success = True
-            y_start = height_map[x_start + 1 - STARTX, z_start + 1 - STARTZ]
+            y_start = height_map[x_start - STARTX, z_start - STARTZ]
     print('Start location: ', x_start, y_start, z_start)
     return x_start, y_start, z_start
 
 
 def clear_plot(house_area, house_level, STARTY, ENDY):
-    GEO.placeVolume(house_area[0, 0] + 1, house_level, house_area[1, 0] + 1, house_area[0, 1], ENDY - 1, house_area[1, 1], blocks = 'air')
-    GEO.placeVolume(house_area[0, 0] + 1, STARTY, house_area[1, 0] + 1, house_area[0, 1], house_level - 1, house_area[1, 1], blocks = 'dirt', replace = ['minecraft:air', 'minecraft:water', 'minecraft:lava'])
+    GEO.placeVolume(house_area[0, 0], house_level, house_area[1, 0], house_area[0, 1], ENDY - 1, house_area[1, 1], blocks = 'air')
+    GEO.placeVolume(house_area[0, 0], STARTY, house_area[1, 0], house_area[0, 1], house_level - 1, house_area[1, 1], blocks = 'dirt', replace = ['minecraft:air', 'minecraft:water', 'minecraft:lava'])
 
 
 def pick_plot(house_size, height_map, STARTX, STARTZ, x_start, y_start, z_start):
     convolution_array = np.ones(house_size)
-    look_area = np.array([[max(STARTX, x_start - house_size[0] + 1 + 1), min(STARTX + height_map.shape[0], x_start + house_size[0] - 1 + 1 + 1)], [max(STARTZ, z_start - house_size[1] + 1 + 1), min(STARTZ + height_map.shape[1], z_start + house_size[1] - 1 + 1 + 1)]])
+    look_area = np.array([[max(STARTX, x_start - house_size[0] + 1), min(STARTX + height_map.shape[0], x_start + house_size[0] - 1 + 1)], [max(STARTZ, z_start - house_size[1] + 1), min(STARTZ + height_map.shape[1], z_start + house_size[1] - 1 + 1)]])
     look_area_height_map = height_map[(look_area[0, 0] - STARTX):(look_area[0, 1] - STARTX), (look_area[1, 0] - STARTZ):(look_area[1, 1] - STARTZ)]
-    look_area = look_area - 1
     look_area_height_map_gradient = look_area_height_map - y_start
     print(look_area_height_map_gradient.shape)
     look_area_height_map_convolved = convolve2d(np.abs(look_area_height_map_gradient), convolution_array, mode = 'valid')
@@ -37,3 +36,29 @@ def pick_plot(house_size, height_map, STARTX, STARTZ, x_start, y_start, z_start)
     house_level = y_start
 
     return house_area, house_level
+
+
+def get_adjacent_points(point_x, point_z, STARTX, STARTZ, ENDX, ENDZ):
+    adjacent_point_list = []
+    if point_x > STARTX:
+        adjacent_point_list.append([point_x - 1, point_z])
+    if point_x <= ENDX:
+        adjacent_point_list.append([point_x + 1, point_z])
+    if point_z > STARTZ:
+        adjacent_point_list.append([point_x, point_z - 1])
+    if point_z <= ENDZ:
+        adjacent_point_list.append([point_x, point_z + 1])
+    return adjacent_point_list
+
+
+def get_path_cost(point_x, point_z, differential, edge_costs):
+    if differential[0] != 0:
+        if differential[0] == 1:
+            return edge_costs[0][point_x, point_z]
+        else:
+            return edge_costs[0][point_x - 1, point_z]
+    else:
+        if differential[1] == 1:
+            return edge_costs[1][point_x, point_z]
+        else:
+            return edge_costs[1][point_x, point_z - 1]
