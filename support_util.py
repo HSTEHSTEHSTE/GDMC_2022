59e42ae3135bf12a358b8f1cce760e7ac7a1cb94
Henry Li <xli257@jhu.edu>
2022-06-02 18:15:44 -0400

    Implement read building into schema

---
+++ support_util.py
@@ -0,0 +1,32 @@
+from gdpc.direct_interface import getBlock, runCommand
+import requests
+from requests.exceptions import ConnectionError
+
+
+def get_block_with_state(x, y, z):
+    """**Return the block ID from the world**."""
+    url = f'http://localhost:9000/blocks?x={x}&y={y}&z={z}&includeState=true'
+    try:
+        response = requests.get(url).text
+    except ConnectionError:
+        return "minecraft:void_air"
+    return response
+
+
+# all boundaries inclusive
+def load_as_blueprint(house_area, house_level, house_level_max):
+    blueprint = {}
+
+    for x in range(house_area[0, 0], house_area[0, 1] + 1):
+        for y in range(house_level, house_level_max + 1):
+            for z in range(house_area[1, 0], house_area[1, 1] + 1):
+                block = get_block_with_state(x, y, z)[10:]
+                
+                if block != 'air[]':
+                    if block in blueprint:
+                        blueprint[block].append((x, y, z))
+                    else:
+                        blueprint[block] = [(x, y, z)]
+
+    print(blueprint)
+    return blueprint
