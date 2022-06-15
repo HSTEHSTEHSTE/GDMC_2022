bfe138fbd88633107817a1b3093275fde96bbac3
Henry Li <xli257@jhu.edu>
2022-06-14 21:58:08 -0400

    Update read blueprint script

--- read_house_blueprint.py
+++ read_house_blueprint.py
@@ -41,6 +41,31 @@ import numpy as np
 # house_level = 1
 # house_level_max = 8
 
+# ## 1b_5 GDMC 5
+# house_area = np.asarray([[-3, 3], [-200, -187]])
+# house_level = 1
+# house_level_max = 11
+
+# ## 1b_6 GDMC 5
+# house_area = np.asarray([[39, 45], [-255, -249]])
+# house_level = 1
+# house_level_max = 4
+
+# ## 1b_7 GDMC 5
+# house_area = np.asarray([[25, 31], [-245, -238]])
+# house_level = 1
+# house_level_max = 5
+
+# ## 1b_8 GDMC 5
+# house_area = np.asarray([[-11, -3], [-244, -235]])
+# house_level = 1
+# house_level_max = 8
+
+## 1b_9 GDMC 5
+house_area = np.asarray([[-29, -21], [-230, -224]])
+house_level = 1
+house_level_max = 6
+
 # ## 2b_0
 # house_area = np.asarray([[101, 109], [-2, 6]])
 # house_level = 1
@@ -66,12 +91,12 @@ import numpy as np
 # house_level = 1
 # house_level_max = 24
 
-# grand_0
-house_area = np.asarray([[-453, -405], [-493, -459]])
-house_level = 7
-house_level_max = 40
+# # grand_0
+# house_area = np.asarray([[-453, -405], [-493, -459]])
+# house_level = 7
+# house_level_max = 40
 
-blueprint = load_as_blueprint(house_area, house_level, house_level_max, house_level_base = 7)
+blueprint = load_as_blueprint(house_area, house_level, house_level_max, house_level_base = 1)
 
 blueprint_file = open("blueprint.txt", "w")
 print(blueprint, file = blueprint_file)
